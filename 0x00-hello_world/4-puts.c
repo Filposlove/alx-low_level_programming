feac4de95740d9827f9e94c4b5bc28582f107522
Filposlove <mergetafilpos@gmail.com>
2023-09-07 14:11:58 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Retur: Always 0 (Succes)
+ */
+int main(void)
+{
+put ("\"Programming is like building a multilingual puzzle");
+
+return (0);
+}
