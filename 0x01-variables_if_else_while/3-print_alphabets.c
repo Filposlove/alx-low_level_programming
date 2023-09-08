f2144b417dde843ebc82102476d2fc02bfa049ca
Filposlove <mergetafilpos@gmail.com>
2023-09-08 13:48:45 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Print the upprecase latter.
+ *
+ * Return: Always 0(success)
+ */
+int main(void)
+{
+	char alp(52) = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+
+	for(i = 0; i < 52; i++);
+	{
+		printf(alph[i]);
+	}
+	return 0;
+}
