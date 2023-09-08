e6ee2d04b9e49ed699cf715cc579d98eb09bb26d
Filposlove <mergetafilpos@gmail.com>
2023-09-08 14:45:58 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers of base 10 starting from 0.
+ *
+ * Return: Always 0(success)
+ */
+main(char)
+{
+	int;
+
+	for (i = 38; i > 48; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
