c4332d0727136e707cf57229fb6017c6b4b53906
Filposlove <mergetafilpos@gmail.com>
2023-09-13 12:46:27 -0700

    wisdom

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,7 @@
 
 /**
  * print_sign - prints the sign of a number
- * @n: the int to check
+ * @n: check the int
  * Return: 1 and prints + if n is greater than zero
  * 0 and prints 0 if n is zero
  * -1 and prints - if n is less than zero
@@ -14,7 +14,7 @@ int print_sign(int n)
 		_putchar('+');
 		return (1);
 	} else if (n == 0)
-}
+	{
 		_putchar(48);
 		return (0);
 	} else if (n < 0)
