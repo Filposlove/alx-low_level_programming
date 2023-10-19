1b7a40bbddec805ae80d5589119982167d280f8c
Filposlove <mergetafilpos@gmail.com>
2023-10-19 11:19:25 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/main.h
+++ 0x12-singly_linked_lists/main.h
@@ -1,7 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
 
-_putchar(char c);
+_putchar.c(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
