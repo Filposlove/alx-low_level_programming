873ccc8c4f6cc3a03406f1b8cdf9060f0c20b467
Filposlove <mergetafilpos@gmail.com>
2023-10-19 11:34:40 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,6 +11,10 @@ size_t print_list(const list_t *h)
 		life = 0;
 	while (h != NULL)
 	{
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 		life++;
 	}
