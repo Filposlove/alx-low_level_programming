7e259f3ce36cea6ee543dea71a7ed6c93d8a5ff0
Filposlove <mergetafilpos@gmail.com>
2023-10-19 10:51:22 -0700

    0-print_list.c

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,40 @@
+iinclude "main.h"
+
+int check_pal(char *s, int i, int len);
+int _strlen_recursion(char *s);
+
+/**
+ * is_palindrome - 1 if a string is a palindrome and 0 if not.
+ * @s: string to reverse
+ *
+ * Return: 1, 0 it's not
+ */
+int is_palindrome(char *s)
+{
+	if (*s == 0)
+		return (1);
+	return (check_pal(s, 0, _strlen_recursion(s)));
+}
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+}
+
+/**
+ * check_pal - 1 if a string is a palindrome and 0 if not.
+ * @s: check
+ * @i: iterator
+ * @len: length of the string
+ *
+ * Return: 1, 0 if not
+ */
+int check_pal(char *s, int i, int len)
+{
+	if (*(s + i) != *(s + len - 1))
+		return (0);
+	if (i >= len)
+		return (1);
+	return (check_pal(s, i + 1, len - 1));
+}
