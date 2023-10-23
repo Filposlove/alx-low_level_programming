be594b70f0112d3aebf013a1e9c2b37e91a47467
Filposlove <mergetafilpos@gmail.com>
2023-10-23 09:56:03 -0700

    1-listint_len.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * listint_len - the number of elements in a linked listint_t list.
+ * @h: linked listint_t to traverse
+ *
+ * Return: number of nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t x = 0;
+		while (h)
+	{
+		x++;
+		h = h->next;
+	}
+	return (x);
+}
